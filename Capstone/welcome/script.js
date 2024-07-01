96c82ef247f2359780e14735d427aa4d4dfe42a9
Batman22-22 <160787227+Batman22-22@users.noreply.github.com>
2024-07-01 14:52:17 -0400

    added pictures and made the style a bit nicer. will work on other parts later

--- Capstone/welcome/script.js
+++ Capstone/welcome/script.js
@@ -1,3 +1,20 @@
 document.getElementById("button").onclick = function () {
     window.location.href = "../../Capstone/signOrLog";
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = [
+        '../fundraising-calendar.jpg',
+        '../images.jpg',
+        '../Calendar_Image.jpg'
+    ];
+
+    let currentImageIndex = 0;
+
+    function swapBackgroundImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        document.body.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+    }
+
+    setInterval(swapBackgroundImage, 5000);
+});
